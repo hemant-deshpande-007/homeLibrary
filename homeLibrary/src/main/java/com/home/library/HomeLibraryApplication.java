d1c9b144534dbf79223169410e880bf40758aac4
Hemant <deshpandehemant2005@gmail.com>
2019-01-04 15:18:54 -0600

    first commit

---
+++ homeLibrary/src/main/java/com/home/library/HomeLibraryApplication.java
@@ -0,0 +1,14 @@
+package com.home.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLibraryApplication.class, args);
+	}
+
+}
+
